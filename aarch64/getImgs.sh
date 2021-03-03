7c7af0c0dec065d6f82ecd2bd7fbfdafb170b73a
Samuel <54447830+Ausaci@users.noreply.github.com>
2021-03-03 20:39:22 +0800

    Update getImgs.sh

--- aarch64/getImgs.sh
+++ aarch64/getImgs.sh
@@ -1,1 +1,2 @@
-wget $(curl -s https://api.github.com/repos/HoldOnBro/Actions-OpenWrt/releases/32444440 | grep browser_download_url | cut -d '"' -f 4)
+# Tag API of Armbian_Buster Release
+wget $(curl -s https://api.github.com/repos/Ausaci/Auto_Build_OpenWrt/releases/39173651 | grep browser_download_url | cut -d '"' -f 4)
