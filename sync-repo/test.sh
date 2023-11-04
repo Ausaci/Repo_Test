7ddd303b40ef1ac344de257cca562bce6d384df5
Samuel <54447830+Ausaci@users.noreply.github.com>
2023-11-04 23:47:54 +0800

    tidy old files

--- sync-repo/test.sh
+++
@@ -1,5 +0,0 @@
-echo "$(date +"%Y-%m-%d %H:%M:%S Update Begin")"
-docker pull busybox
-docker run --network=bridge --rm -it busybox ifconfig
-echo "$1"
-echo "$(date +"%Y-%m-%d %H:%M:%S Update End")"
